a3c36d133b9865ddade2a0c67afb81e27cddd557
Yasin Cüre <yasindenizcure@gmail.com>
2025-01-22 20:54:15 +0300

    keep goin

---
+++ Entities/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AcunMedyaRestaurantly.Entities
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        [StringLength(250)]
+        public bool IsRead { get; set; }
+        public DateTime SendDate { get; set; }
+        public string Message { get; set; }
+    }
+}
