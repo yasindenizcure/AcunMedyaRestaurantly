854f46d72b9588660ea787c935c3c85c322293c3
Yasin Cüre <yasindenizcure@gmail.com>
2025-01-22 21:53:00 +0300

    CategoryController & Index

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using AcunMedyaRestaurantly.Context;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedyaRestaurantly.Controllers
+{
+    public class CategoryController : Controller
+    {
+        RestaurantlyContext db = new RestaurantlyContext();
+        public ActionResult Index()
+        {
+            var value = db.Categories.ToList();
+            return View(value);
+        }
+    }
+}
