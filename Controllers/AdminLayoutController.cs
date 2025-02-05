789073f09cf03acd3b5b3c220c881d018157c02e
Yasin Cüre <yasindenizcure@gmail.com>
2025-02-05 22:02:14 +0300

    05.02.2025 adım 8

---
+++ Controllers/AdminLayoutController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedyaRestaurantly.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        // GET: AdminLayout
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PartialHead()
+        {
+            return PartialView();
+        }
+        public ActionResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public ActionResult PartialSidebar()
+        {
+            return PartialView();
+        }
+        public ActionResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public ActionResult PartialScript()
+        {
+            return PartialView();
+        }
+    }
+}
--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using AcunMedyaRestaurantly.Context;
+using AcunMedyaRestaurantly.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -16,6 +17,44 @@ namespace AcunMedyaRestaurantly.Controllers
         {
             var value = db.Categories.ToList();
             return View(value);
+        }
+        public ActionResult CategoryList()
+        {
+            var value = db.Categories.ToList();
+            return View(value);
+        }
+        [HttpGet]
+        public ActionResult CategoryCreate() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CategoryCreate(Category model)
+        {
+            db.Categories.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+        [HttpGet]
+        public ActionResult CategoryEdit(int id)
+        {
+            var value = db.Categories.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult CategoryEdit(Category model)
+        {
+            var values = db.Categories.Find(model.CategoryId);
+            values.CategoryName = model.CategoryName;
+            db.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+        public ActionResult CategoryDelete(int id)
+        {
+            var value = db.Categories.Find(id);
+            db.Categories.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("CategoryList");
         }
     }
 }
