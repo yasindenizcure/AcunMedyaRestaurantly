07708f40bd5a520739b4fd76cfb3f5bbf4a963c6
Yasin Cüre <yasindenizcure@gmail.com>
2025-03-04 03:41:55 +0300

    Testimonial

---
+++ Controllers/TestimonialController.cs
@@ -0,0 +1,68 @@
+﻿using AcunMedyaRestaurantly.Context;
+using AcunMedyaRestaurantly.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedyaRestaurantly.Controllers
+{
+    [Authorize]
+    public class TestimonialController : Controller
+    {
+        RestaurantlyContext db = new RestaurantlyContext();
+        public ActionResult Index()
+        {
+            var value = db.Testimonials.ToList();
+            return View(value);
+        }
+        public ActionResult TestimonialList(string searchText)
+        {
+            List<Testimonial> values;
+            if (searchText != null)
+            {
+                values = db.Testimonials.Where(x => x.Name.Contains(searchText)).ToList();
+                return View(values);
+            }
+            var value = db.Testimonials.ToList();
+            return View(value);
+        }
+        [HttpGet]
+        public ActionResult TestimonialCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TestimonialCreate(Testimonial model)
+        {
+            db.Testimonials.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("TestimonialList");
+        }
+        [HttpGet]
+        public ActionResult TestimonialEdit(int id)
+        {
+            var value = db.Testimonials.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult TestimonialEdit(Testimonial model)
+        {
+            var value = db.Testimonials.Find(model.TestimonialId);
+            value.Name = model.Name;
+            value.Title = model.Title;
+            value.Description = model.Description;
+            value.ImageUrl = model.ImageUrl;
+            db.SaveChanges();
+            return RedirectToAction("TestimonialList");
+        }
+        public ActionResult TestimonialDelete(int id)
+        {
+            var value = db.Testimonials.Find(id);
+            db.Testimonials.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("TestimonialList");
+        }
+    }
+}
